757f943aa8cb3add133ebab0e5083cb7bb17996a
Gurvinder Singh Sandhu <Gurvinder.Singh.Sandhu@gmail.com>
2017-01-04 14:34:25 +0000

    added alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,14 @@
 console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "king";
-var cardThree = "king"
-var cardFour = "queen"
+var cardThree = "queen"
+var cardFour = "king"
 cardOne == cardFour;
+
+if (cardTwo === cardFour){
+	alert('You found a match!');
+}
+
+if (cardOne === cardThree){
+	alert('You found a match!');
+} 
