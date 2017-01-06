7a11ac6a93c9f58cc1e2adc9e4982060c1fa99f7
Gurvinder Singh Sandhu <Gurvinder.Singh.Sandhu@gmail.com>
2017-01-06 11:48:08 +0000

    added JS function createCards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,21 @@ var cardThree = "queen"
 var cardFour = "king"
 cardOne == cardFour;
 
-if (cardTwo === cardFour){
-	alert('You found a match!');
+// if (cardTwo === cardFour){
+// 	alert('You found a match!');
+// }
+
+// if (cardOne === cardThree){
+// 	alert('You found a match!');
+// } 
+
+var createCards = function() {
+	var gameBoard = document.getElementById('game-board');
+		for (var i=0; i<4; i++) {
+		  var newCard = document.createElement('div');
+		  newCard.innerHTML = '<div class="card"></div>';
+		  gameBoard.appendChild(newCard);
+		}
 }
 
-if (cardOne === cardThree){
-	alert('You found a match!');
-} 
+createCards();
