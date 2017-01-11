a4ae2e8021b2568e80dd5c066a503f9c0a14b7dd
Gurvinder Singh Sandhu <Gurvinder.Singh.Sandhu@gmail.com>
2017-01-11 17:41:00 +0000

    created isTwoCards & isMatch functions - game completed using alerts for testing - needs cleaning up

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,32 +1,67 @@
 console.log("JS file is connected to HTML! Woo!")
-// var cardOne = "queen";
-// var cardTwo = "king";
-// var cardThree = "queen"
-// var cardFour = "king"
-// cardOne == cardFour;
 
-var cards = ['queen', 'queen', 'king', 'king'];
-
+var cards = ['queen', 'queen', 'king', 'king', 'Guv', 'Nya'];
 var cardsInPlay = [];
 
-var createCards = function() {
+var createBoard = function() 
+{
 	var gameBoard = document.getElementById('game-board');
-		for (var i=0; i<6; i++) {
-		  var newCard = document.createElement('div');
-		  newCard.innerHTML = '<div class="card"></div>';
-		  gameBoard.appendChild(newCard);
-		}
+		for (var i=0; i<cards.length; i++) 
+		{
+		  var cardElement = document.createElement('div');
+		  cardElement.innerHTML = '<div class="card"></div>';
+		  // this will set the card's 'data-card' to be the element of the array
+		  cardElement.setAttribute('data-card', cards[i]);
+		  cardElement.addEventListener('click', isTwoCards);
+		  gameBoard.appendChild(cardElement);
+		 }
 }
 
-createCards();
+// checks to see if there are cards in play
+var isTwoCards = function() 
+{
+	cardsInPlay.push(this.getAttribute('data-card'));
+	window.alert(cardsInPlay);
+
+	if (cardsInPlay.length === 2)
+	{
+		window.alert("you have 2 cards in play");
+		isMatch(cardsInPlay);
+	}
+}
+
+var isMatch = function(cIP)
+{
+	if (cIP[0] === cIP[1])
+	{
+		window.alert("You found a match...!");
+	} else 
+	{
+		window.alert("Sorry, try again...");
+	}
+}
+
+createBoard();
 
 
-// Requirements
 
-// Replace all of your card variables with an array of cards. 
-// Once that is complete, organize all your code into functions. 
-// Then, create two more functions; one should check if two cards 
-// are in play, and the other should check if two cards are a match.
+// //checks to see if there are cards in play
+// function isTwoCards() {
 
-// 1) Declare a variable, cards, and set it equal to an array of four string elements: 'queen', 'queen', 'king', 'king'.
-// 2) Declare a variable, cardsInPlay, whose value is an empty array.
+//   // add card to array of cards in play
+//   // 'this' hasn't been covered in this pre-work, but
+//   // for now, just know it gives you access to the card the user clicked on
+//   cardsInPlay.push(this.getAttribute('data-card'));
+
+//   // if you have two cards in play, check for a match
+//   if (cardsInPlay.length === 2) {
+
+//     // pass the cardsInPlay as an argument to the isMatch function
+//     isMatch(cardsInPlay);
+
+//     // clear cards in play array for your next try
+//     cardsInPlay = [];
+
+//   }
+
+// }
