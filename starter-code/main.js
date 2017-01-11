c9a7040fc9d06d04f5358a618fcbb94970c651dc
Gurvinder Singh Sandhu <Gurvinder.Singh.Sandhu@gmail.com>
2017-01-11 11:17:16 +0000

    created empty array called cardsInPlay

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,6 +7,8 @@ console.log("JS file is connected to HTML! Woo!")
 
 var cards = ['queen', 'queen', 'king', 'king'];
 
+var cardsInPlay = [];
+
 var createCards = function() {
 	var gameBoard = document.getElementById('game-board');
 		for (var i=0; i<6; i++) {
@@ -27,3 +29,4 @@ createCards();
 // are in play, and the other should check if two cards are a match.
 
 // 1) Declare a variable, cards, and set it equal to an array of four string elements: 'queen', 'queen', 'king', 'king'.
+// 2) Declare a variable, cardsInPlay, whose value is an empty array.
