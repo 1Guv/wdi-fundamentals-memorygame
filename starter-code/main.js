5f6271911db4c7d52599d85ad7f1a74315697ab3
Gurvinder Singh Sandhu <Gurvinder.Singh.Sandhu@gmail.com>
2017-01-14 21:03:17 +0000

    added comments - this has been completed

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,27 +3,24 @@ console.log("JS file is connected to HTML! Woo!")
 var cards = ['queen', 'queen', 'king', 'king', 'joker', 'joker'];
 var cardsInPlay = [];
 
-var createBoard = function() 
+var createBoard = function() // creates the Board from the cards array so with 6 cards
 {
-	var gameBoard = document.getElementById('game-board');
-		for (var i=0; i<cards.length; i++) 
+	var gameBoard = document.getElementById('game-board'); // stores the HTML DIV id for the starting location for the game board
+		for (var i=0; i<cards.length; i++) // goes through the array to append them to the board
 		{
-		  var cardElement = document.createElement('div');
-		  cardElement.innerHTML = '<div class="card"></div>';
-		  // this will set the card's 'data-card' to be the element of the array
-		  cardElement.setAttribute('data-card', cards[i]);
-		  cardElement.addEventListener('click', isTwoCards);
-		  gameBoard.appendChild(cardElement);
+		  var cardElement = document.createElement('div'); // creating a div code and stored as a variable
+		  cardElement.innerHTML = '<div class="card"></div>'; // the actual HTML to be appended to the HTML
+		  cardElement.setAttribute('data-card', cards[i]); // this will set the card's 'data-card' to be the element of the array
+		  cardElement.addEventListener('click', isTwoCards); // on click the isTwoCards function is invoked
+		  gameBoard.appendChild(cardElement); // appending the HTML to the game board
 		 }
 }
 
-// checks to see if there are cards in play
-var isTwoCards = function() 
+var isTwoCards = function() // checks to see if there are cards in play
 {
 	var dataCard = this.getAttribute('data-card');
 	cardsInPlay.push(dataCard);
-	// used the below for testing the output
-	// window.alert(cardsInPlay);
+	// window.alert(cardsInPlay); // used for testing the output
 
 	// show card pictures
 	if (dataCard === 'king') 
@@ -40,35 +37,33 @@ var isTwoCards = function()
 
 	if (cardsInPlay.length === 2)
 	{
-		// used the below for testing the output
-		// window.alert("you have 2 cards in play");
+		// window.alert("you have 2 cards in play"); // used for testing the output
 		isMatch(cardsInPlay);
 		cardsInPlay = [];
 
 	}
 }
 
-var isMatch = function(cIP)
+var isMatch = function(cIP) // CIP is short for cards in play
 {
-	if (cIP[0] === cIP[1])
+	if (cIP[0] === cIP[1]) // comparing the cards in the array to see if they match
 	{
 		window.alert("You found a match...!");
 	} else 
 	{
 		window.alert("Sorry, try again...");
-		clearBoard();
+		clearBoard(); // invokes a function to clear the board
 	}
 }
 
-var clearBoard = function()
+var clearBoard = function() // function to clear the board when there is a match
 {
 	// window.alert("clearBoard 1"); // used this for testing code
-	var x = document.getElementsByClassName('card');
+	var x = document.getElementsByClassName('card'); // array of all elements using the Class name so the HTML can be cleared
 	for (var i = 0; i < x.length; i++) 
 	{
-    	x[i].innerHTML = "";
+    	x[i].innerHTML = ""; // clear the HTML so the board starts over
 	}
-	// window.alert("clearBoard 2"); // used this for testing
 }
 
 createBoard();
